c80669ac12d7793e01449b7c634661b690358b2a
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-03-10 11:56:00 +0700

    feat: Create text component

---
+++ src/components/typography/text/index.tsx
@@ -0,0 +1,73 @@
+import styled from "@emotion/styled";
+import { noop } from "../../../const";
+import { ETextAlign, templateStringToClassName } from "../../../core";
+import EContentTypeTypography from "../../../core/types/EContentTypeTypography";
+import { Copyable, Editable, Ellipsis } from "../general-types";
+import { default as TextCustom } from "./text";
+
+const StyledText = styled(TextCustom)``;
+const StyledWrapText = styled("div")``;
+
+export interface TextProps {
+  //   delete // TODO Missing type
+  code?: boolean;
+  copyable?: boolean | Copyable;
+  disabled?: boolean;
+  editable?: boolean | Editable;
+  ellipsis?: boolean | Omit<Ellipsis, "expandable" | "rows" | "onExpand">;
+  italic?: boolean;
+  keyboard?: boolean;
+  mark?: boolean;
+  strong?: boolean;
+  textAlign?: ETextAlign;
+  type?: EContentTypeTypography;
+  underline?: boolean;
+  onClick?: (event: any) => void;
+}
+
+export function Text({
+  code = false,
+  copyable = false,
+  disabled = false,
+  editable = false,
+  ellipsis = false,
+  italic = false,
+  keyboard = false,
+  mark = false,
+  strong = false,
+  textAlign,
+  type,
+  underline = false,
+  onClick = noop,
+}: TextProps) {
+  const passProps = {
+    code,
+    copyable,
+    disabled,
+    editable,
+    ellipsis,
+    italic,
+    keyboard,
+    mark,
+    strong,
+    type,
+    underline,
+    onClick,
+  };
+
+  const textElement = <StyledText {...passProps}></StyledText>;
+
+  return textAlign ? (
+    <StyledWrapText
+      className={templateStringToClassName()`
+          text-align: ${textAlign}
+    `}
+    >
+      {textElement}
+    </StyledWrapText>
+  ) : (
+    { textElement }
+  );
+}
+
+export default Text;
