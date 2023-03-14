296743ac3ce680add5822db22c5299a01ec380f1
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-03-14 08:03:16 +0700

    feat: Create prodcut item layout

---
+++ src/dummy-data/index.tsx
@@ -0,0 +1,64 @@
+import { LaptopCardItemProps } from "../components/laptop-card-item";
+
+export const laptopItemList: LaptopCardItemProps[] = [
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+  {
+    laptopCurrency: 1000000,
+    laptopName: "Lenovo Legion 5 Pro 2022",
+    srcImage:
+      "https://media-api-beta.thinkpro.vn/media/core/products/2022/10/1/2375_lenovo_legion_5_pro_16iah7h_ct1_1600.png?w=500&h=500",
+  },
+];
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,8 @@
-
+import ProductItemLayout from "../../compositions/product-item-layout/product-item-layout";
+import { laptopItemList } from "../../dummy-data";
 
 function HomePage() {
-  return <div>This is home page</div>;
+  return <ProductItemLayout children={laptopItemList} />;
 }
 
 export default HomePage;
