20d28d968fdfb6a6717071809dafa80d076be025
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-04-15 09:25:53 +0700

    refactor: Refactor structure api in core

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,26 +1,2 @@
-import { API_SPRING_BOOT, PROVINCES_API_LINK } from "@constant";
-import axios from "axios";
-import { ILogin } from "../types";
-
-export const apiProvinces = () =>
-  axios.get(PROVINCES_API_LINK).then((res) => res.data);
-
-const instanceAxios = axios.create({
-  baseURL: API_SPRING_BOOT,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "POST",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, X-Requested-With",
-  },
-});
-
-export const apiLogin = ({ username, password }: ILogin) => {
-  return instanceAxios
-    .post("/auth/login", {
-      username,
-      password,
-    })
-    .then((res) => res.data);
-};
+export * from "./auth";
+export * from "./provinces";
