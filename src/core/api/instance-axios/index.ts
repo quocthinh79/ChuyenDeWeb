9f614fbd6ff24b1cd6cb47a1d301425ac07164bf
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-05-25 14:05:36 +0700

    refactor: Refactor Authorization header

--- src/core/api/instance-axios/index.ts
+++ src/core/api/instance-axios/index.ts
@@ -4,7 +4,6 @@ import axios from "axios";
 const instanceAxios = axios.create({
   baseURL: API_SPRING_BOOT,
   headers: {
-    authorization: "authorization-text",
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
