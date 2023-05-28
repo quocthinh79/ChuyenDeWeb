f162152107b7ece3672009d43c6688efb4872d12
quocthinh79 <19130215@st.hcmuaf.edu.vn>
2023-05-29 06:07:09 +0700

    fix: Fix bug and add messager

--- src/compositions/account/right-content/index.tsx
+++ src/compositions/account/right-content/index.tsx
@@ -11,8 +11,10 @@ import { useForm } from "antd/es/form/Form";
 import AddressInformationFormItem from "./address-information-form-item";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useStorageToken } from "@store";
+import { notification } from "antd";
 
 export function AccountRightContent() {
+  const [api, contextHolder] = notification.useNotification();
   const { token } = useStorageToken();
   const [form] = useForm();
   const onFinish = ({ city, district, ward }: any) => {
@@ -40,12 +42,17 @@ export function AccountRightContent() {
     mutationKey: ["account"],
     mutationFn: apiUpdateAccount,
     onSuccess: ({ address }) => {
-      const [city, district, ward] = address?.split(", ") || [];
+      const [ward, district, city] = address?.split(", ") || [];
+      console.log(address);
 
       form.setFieldsValue({
         city,
         district,
         ward,
+      });
+      api["success"]({
+        message: "Thành Công",
+        description: "Đã cập nhật thành công",
       });
     },
     onError: (error) => {
@@ -58,30 +65,33 @@ export function AccountRightContent() {
   };
 
   return (
-    <Card>
-      <Title level={2}>Địa chỉ mặc định</Title>
-      <Form
-        form={form}
-        layout="vertical"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Space
-          size={SizeProps.Large}
-          direction={EDirectionType.Vertical}
-          widthFull
+    <>
+      {contextHolder}
+      <Card>
+        <Title level={2}>Địa chỉ mặc định</Title>
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
-          <AddressInformationFormItem />
-          <Button
-            block
-            htmlType={EHtmlButtonTypes.Submit}
-            type={EButtonTypes.Primary}
+          <Space
+            size={SizeProps.Large}
+            direction={EDirectionType.Vertical}
+            widthFull
           >
-            Cập nhật
-          </Button>
-        </Space>
-      </Form>
-    </Card>
+            <AddressInformationFormItem />
+            <Button
+              block
+              htmlType={EHtmlButtonTypes.Submit}
+              type={EButtonTypes.Primary}
+            >
+              Cập nhật
+            </Button>
+          </Space>
+        </Form>
+      </Card>
+    </>
   );
 }
 
